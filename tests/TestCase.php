a534ca635dabcbad24b2a207113f3f6b1c0ab5d1
Frank Fava <frank.fava@gmail.com>
2025-01-16 18:05:37 +0100

    Test Suite Setup

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,29 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Resources\Json\JsonResource;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use RefreshDatabase, WithFaker;
+
+    /**
+     * Decode Response
+     */
+    protected function content($response, $asArray = true)
+    {
+        return json_decode($response->content(), !!$asArray) ?? $response->content();
+    }
+
+    /**
+     * Get Original Response
+     */
+    protected function original($response)
+    {
+        $original = $response->original;
+        return ($original instanceof JsonResource) ? $original->resource : $original;
+    }
 }
