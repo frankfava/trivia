98394c27dd974ea3b7985580f8373bff7496ff9c
Frank Fava <frank.fava@gmail.com>
2025-01-17 01:24:37 +0100

    Fetch Trivia Question Via API

---
+++ app/Actions/FetchTriviaQuestions.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Actions;
+
+use App\Enums\QuestionDifficulty;
+use App\Enums\QuestionType;
+use App\Models\Question;
+use Closure;
+use Illuminate\Support\Facades\Http;
+
+class FetchTriviaQuestions
+{
+    const API_URL = 'https://opentdb.com/api.php?';
+
+    protected ?Closure $afterEachTry;
+    protected ?Closure $onCompletion;
+
+    public function __construct(
+        readonly protected ?int $totalQuestions = 1000,
+        readonly protected ?QuestionDifficulty $difficulty = null,
+        readonly protected ?QuestionType $type = null,
+        readonly protected int $questionsPerBatch = 50,
+        ?Closure $afterEachTry = null,
+        ?Closure $onCompletion = null,
+    ) {
+        $this->afterEachTry = $afterEachTry ?: null;
+        $this->onCompletion = $onCompletion ?: null;
+    }
+
+    public function execute()
+    {
+        $fetchedQuestions = 0;
+
+        while ($fetchedQuestions < $this->totalQuestions) {
+            $response = Http::get(self::API_URL, array_filter([
+                'amount' => $this->questionsPerBatch,
+                'difficulty' => $this->difficulty->value,
+                'type' => $this->type->value,
+            ]));
+            $json = $response->json();
+
+            if ($json['response_code'] != 0) {
+                continue;
+            }
+
+            $questions = $json['results'] ?? [];
+
+            if (!empty($questions)) {
+                foreach ($questions as $question) {
+                    if ($fetchedQuestions >= $this->totalQuestions) {
+                        break;
+                    }
+                    if ($this->storeQuestion($question)) {
+                        $fetchedQuestions++;
+                    }
+                }
+            }
+
+            if ($this->afterEachTry instanceof Closure) {
+                call_user_func($this->afterEachTry, $fetchedQuestions, count($questions));
+            }
+        }
+
+        if ($this->onCompletion instanceof Closure) {
+            call_user_func($this->onCompletion, $fetchedQuestions);
+        }
+
+        return $fetchedQuestions;
+    }
+
+    protected function storeQuestion(array $questionData)
+    {
+        $contentHash = substr(md5(json_encode($questionData)), 0, 12);
+
+        // Check if question already exists based on the content hash
+        $existingQuestion = Question::where('content_hash', $contentHash)->exists();
+
+        if (!$existingQuestion) {
+            Question::forceCreate([
+                'type' => $questionData['type'],
+                'question' => $questionData['question'],
+                'category_id' => $this->getCategoryId($questionData['category']),
+                'difficulty' => $questionData['difficulty'],
+                'correct_answer' => $questionData['correct_answer'],
+                'incorrect_answers' => count($d = $questionData['incorrect_answers']) == 1 ? $d[0] : $d,
+                'content_hash' => $contentHash,
+            ]);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    protected function getCategoryId($categoryName)
+    {
+        // If Category doesn't exist, create it
+        $category = \App\Models\Category::firstOrCreate(['name' => $categoryName]);
+
+        return $category->id;
+    }
+}
---
+++ app/Console/Commands/FetchTriviaQuestionsCommand.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Actions\FetchTriviaQuestions;
+use Illuminate\Console\Command;
+use Exception;
+
+class FetchTriviaQuestionsCommand extends Command
+{
+    protected $signature = 'fetch:trivia-questions 
+                            {--total=1000 : Number of questions to fetch}
+                            {--difficulty= : The difficulty level of the questions (easy, medium, hard)}
+                            {--type= : The type of question (multiple, boolean)} 
+                            {--batch=50 : The number of questions to fetch per API request (max 50)}';
+
+    protected $description = 'Fetch trivia questions from Open Trivia Database and store them in the database';
+
+    public function handle()
+    {
+        try {
+            $totalQuestions = $this->option('total');
+            $difficulty = $this->option('difficulty');
+            $type = $this->option('type');
+            $questionsPerBatch = $this->option('batch');
+
+            // Validate that questionsPerBatch is not greater than 50
+            if ($questionsPerBatch > 50) {
+                throw new Exception('The number of questions per batch cannot exceed 50.');
+            }
+
+            $this->info("Fetching $totalQuestions trivia questions...");
+
+            $fetchTriviaQuestionsAction = new FetchTriviaQuestions(
+                totalQuestions: $totalQuestions,
+                difficulty: $difficulty,
+                type: $type,
+                questionsPerBatch: $questionsPerBatch,
+                afterEachTry: function ($aggregateCount, $fetchedCount) {
+                    $this->line("Fetched $fetchedCount questions. Total: $aggregateCount");
+                },
+                onCompletion: fn($fetchedCount) => $this->info("Fetched $fetchedCount questions.")
+            );
+
+            $fetchedCount = $fetchTriviaQuestionsAction->execute();
+
+            $this->info("Successfully fetched trivia questions.");
+        } catch (Exception $e) {
+            $this->error('Error: ' . $e->getMessage());
+        }
+    }
+}
